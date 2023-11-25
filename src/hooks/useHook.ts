6a14cf854c1ab177729d233b35d9e893b33f3d4f
Adarsha <adarsha.sagar183@gmail.com>
2023-11-25 20:46:44 +0530

    custom hook for fetching games

---
+++ src/hooks/useHook.ts
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+import apiClient from "../services/api-client";
+
+interface Game {
+  id: number;
+  name: string;
+}
+
+interface Gamegetresponse {
+  count: number;
+  results: Game[];
+}
+
+const useHooks = () => {
+  const [games, setGames] = useState<Game[]>([]);
+  const [error, setError] = useState("");
+  useEffect(() => {
+    const controller = new AbortController();
+    apiClient
+      .get<Gamegetresponse>("/games", { signal: controller.signal })
+      .then((res) => setGames(res.data.results))
+      .catch((err) => setError(err.message));
+  }, []);
+  return { games, error };
+};
+export default useHooks;
